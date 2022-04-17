21d91732c5af60979bb984675f2b8573a376cdd9
Justin Moye <justin@Justins-MacBook-Pro.local>
2022-04-16 20:44:21 -0400

    Added Grid Squares

---
+++ main.js
@@ -0,0 +1,18 @@
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll('.div')
+    
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for(let i = 0; i < 256; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'blue';
+        board.insertAdjacentElement('beforeend', square);
+}
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    populateBoard(input)
+}
